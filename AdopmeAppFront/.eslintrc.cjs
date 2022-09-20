99c61b1665adbc37ff639c5417d5519dfb627824
Yeminson Sanchez <yeminsons@gmail.com>
2022-09-19 22:33:59 -0300

    create project, structure folders, config prettier and eslint

---
+++ AdopmeAppFront/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard',
+    'eslint-config-prettier'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'object-curly-spacing': 0,
+    anonymous: 0,
+    named: 0
+  }
+}
